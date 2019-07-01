cb92f78725f975505f368f1f1aca0ff556762862
wangonya <kwangonya@gmail.com>
2019-07-01 09:37:13 +0300

    remove redux-dev-tools

--- src/store.js
+++ src/store.js
@@ -1,17 +1,10 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
 
 const initialState = {};
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
 export default store;
